19ccb01b5c44647b4e2de15f2058120ff0d0d305
Oliver Zott <zott_oliver@web.de>
2021-10-29 07:42:06 +0200

    Component product-list extended.

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Product} from 'src/app/common/product';
+import {ProductService} from "../../services/product.service";
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
+  // templateUrl: './product-list.component.html',
+  templateUrl: './product-list-table.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products!: Product[];  /* otherwise use "| undefined" or " = []" */
+
+  constructor(private productService: ProductService) {
+  }
 
   ngOnInit(): void {
+    this.listProducts();
+  }
+
+  // Method is invoked once we "subscribe"
+  //   - inside method we assign result to Product[]
+  listProducts() {
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data;
+      }
+    )
   }
 
 }
